53bc3511a0327c71a035cf23d57a3e876a53c769
yondmn <ym_xiaqing@sina.com>
2017-08-23 16:25:09 +0800

    finished the first version

--- README.md
+++ README.md
@@ -1,9 +1,32 @@
 # vuecookie
 
-A Vue.js plugin for operating cookies tested up to ```Vue v2.3.2```
+A Vue.js plugin for operating cookies tested up to ```Vue```
 
 Install through npm
 
 ``` bash
 npm install vuecookie --save
+```
+
+## Usage
+
+The plugin is available through ```this.$cookie``` in Vue components
+
+### For Example
+``` javascript
+
+this.$cookie.set('ym', 18, {expires: 1});
+
+this.$cookie.get('ym');
+
+this.$cookie.delete('ym');
+
+this.$cookie.set('ym', 'One day later', { expires: 1 });
+this.$cookie.set('ym', 'One year later', { expires: '1Y' });
+this.$cookie.set('ym', 'One month later', { expires: '1M' });
+this.$cookie.set('ym', 'One day later', { expires: '1D' });
+this.$cookie.set('ym', 'One hour later', { expires: '1h' });
+this.$cookie.set('ym', 'Ten minutes later', { expires: '10m' });
+this.$cookie.set('ym', 'Thirty seconds later', { expires: '30s' });
+
 ```
--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 ;(function () {
-	
 	/**
 	 * 
 	 * 计算有效期
@@ -7,13 +6,13 @@
 	 * @param {any} expiresStr 有效期数字或字符串
 	 */
 	function calcExpires (expiresStr) {
-		var Years = /^\d+Y$/, Months = /^\d+M$/, Days = /^\d+D$/, hours = /^\d+h$/, minutes = /^\d+m$/, seconds = /^\d+s$/;
+		var Years = /^\d+Y$/, Months = /^\d+M$/, Days = /^[-\d]+D$/, hours = /^\d+h$/, minutes = /^\d+m$/, seconds = /^\d+s$/;
 		// culc time unit
 		var time = new Date(), timeUnit = 0;
 		expiresStr = expiresStr === undefined ? '1D' :
 			+expiresStr + '' == expiresStr ? expiresStr + 'D' : expiresStr;
 
-		switch (expiresStr) {
+		switch (true) {
 			case Years.test(expiresStr):
 				timeUnit = 864e+5 * 365;
 				break;
@@ -36,47 +35,78 @@
 				throw new Error('expires type error!');
 		}
 		expiresStr = expiresStr.replace(/[YMDhms]/, '') * 1;
-		time.setMilliseconds(time.getMilliseconds + expiresStr * timeUnit);
+		time.setMilliseconds(time.getMilliseconds() + expiresStr * timeUnit);
 
-		return time;
+		return time.toGMTString();
 	}
 	
 	function extend () {
 		return Object.assign.apply({}, arguments);
 	}
 	
-	var defaultOptions = {expires: 1};
+	var defaultOptions = {path: '/', expires: 1};
 
 	var VueCookie = {
 		install: function (Vue) {
 			Vue.prototype.$cookie = this;
 			Vue.cookie = this;
 		},
-		set: function (key, value, expires) {
+		set: function (key, value, options) {
+			options = extend(defaultOptions, options);
+			var expires = options.expires;
 			// 纯数字或数字字符串单位默认为天
 			if (typeof expires === 'number' || typeof expires === 'string') {
-				expires = calcExpires(expires);
+				options.expires = calcExpires(expires);
 			} else {
 				throw new Error('expires must be number or string type!');
 			}
 
-			// value to json
+			// value 转换为 json
 			var result = JSON.stringify(value);
 			if (/[\{\[]/.test(result)) {
 				value = result;
 			}
-
+			
+			// 特殊字符转换
 			value = encodeURIComponent(String(value))
 				.replace(/%(23|24|26|2B|3A|3C|3E|3D|2F|3F|40|5B|5D|5E|60|7B|7D|7C)/g, decodeURIComponent);
 
 			key = encodeURIComponent(String(key))
 				.replace(/%(23|24|26|2B|5E|60|7C)/g, decodeURIComponent);
+			
+			var tempStr = '';
+			// 遍历属性
+			for (var val in options) {
+				if (!options[val]) continue;
+				tempStr += '; ' + val + '=' + options[val];
+			}
+
+			document.cookie = key + '=' + value + tempStr;
+			return document.cookie;
 		},
 		get: function (key) {
+			var cookiesArray = document.cookie ? document.cookie.split('; ') : Array.prototype,
+				i = 0,
+				len = cookiesArray.length,
+				item = [],
+				value,
+				tempKey;
 
+			for (i; i < len; i++) {
+				item = cookiesArray[i].split('=');
+				tempKey = decodeURIComponent(item[0]);
+				if (tempKey === key) {
+					value = decodeURIComponent(item[1]);
+					if (/[\{\[]/.test(value)) {
+						value = JSON.parse(value);
+					}
+				}
+			}
+
+			return value;
 		},
-		delete: function (key, options) {
-
+		delete: function (key) {
+			this.set.call(this, key, '', {expires: -1});
 		}
 	};
 
@@ -86,7 +116,7 @@
 		define([], function () {
 			return VueCookie;
 		});
-	} else (window.Vue) {
+	} else if (window.Vue) {
 		window.VueCookie = VueCookie;
 		Vue.use(VueCookie);
 	}
